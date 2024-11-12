ef683dc29285e2f29b19a585b4208c395e05fbe1
vishngar <vishnu.garg@oracle.com>
2024-11-12 14:11:30 +0530

    Project Initial Setup

---
+++ src/main/java/com/farmcollector/FarmCollectorApplication.java
@@ -0,0 +1,13 @@
+package com.farmcollector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmCollectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FarmCollectorApplication.class, args);
+    }
+
+}
