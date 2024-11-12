35f1c25ff834efcf9e1d47819e892626bcb79c7b
vishngar <vishnu.garg@oracle.com>
2024-11-12 14:29:56 +0530

    skeleton changes for planting service and unit test case

--- src/main/java/com/farmcollector/config/SwaggerConfig.java
+++ src/main/java/com/farmcollector/config/SwaggerConfig.java
@@ -2,8 +2,10 @@ package com.farmcollector.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -15,6 +17,15 @@ public class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.farmcollector.controller"))
                 .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiDetails());
+    }
+
+    private ApiInfo apiDetails() {
+        return new ApiInfoBuilder()
+                .title("Farm Collector API")
+                .description("API for collecting and reporting planting and harvesting data from farms")
+                .version("1.0")
                 .build();
     }
 }
